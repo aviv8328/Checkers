a99081611deea9394bd04bd2eeed77a3ce80c2b6
aviv8328 <avivdror20@gmail.com>
2024-06-24 19:28:48 +0300

    ADDED --> First implementaion of get-params

---
+++ Checkers/GetParameters/GetPlayerParameters.cs
@@ -0,0 +1,10 @@
+namespace CheckersHafifa
+{
+    public class PlayerParameters
+    {
+        public string playerName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
