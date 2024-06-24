feb71f89bbb7b6d7fa97d17cf790ec038a3a8a6f
aviv8328 <avivdror20@gmail.com>
2024-06-24 19:32:44 +0300

    ADDED --> First implementaion of board+player validation

---
+++ Checkers/Validation/ValidateGameAttributes.cs
@@ -0,0 +1,15 @@
+namespace CheckersHafifa
+{
+    public class ValidateGameAttributes : IValidate
+    {
+        public bool ValidateBoardSize(int boardSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidatePlayerName(string playerName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
