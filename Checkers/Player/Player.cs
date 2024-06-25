8a746b224d25c6848bd5f9d3365a98180f903603
aviv8328 <avivdror20@gmail.com>
2024-06-25 10:58:19 +0300

    ADDED --> first implementation of player creation

--- Checkers/Player/Player.cs
+++ Checkers/Player/Player.cs
@@ -6,9 +6,20 @@ namespace CheckersHafifa
 
         public List<Piece> pieces { get; }
 
-        public Player(string name)
+        public Player(string name, int boardSize)
         {
-            playerName = name;   
+            playerName = name;
+
+            for (int i = 0; i < ReturnNumberOfPiecesInRow(boardSize); i++)
+            {
+                // create piece based on color
+            }            
+
+        }
+
+        private int ReturnNumberOfPiecesInRow(int boardSize)
+        {
+            return (boardSize / 2) * 3;
         }
     }
 }
